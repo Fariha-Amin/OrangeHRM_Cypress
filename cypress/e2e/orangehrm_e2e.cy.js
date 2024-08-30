f6d51dc87de0888d42372c39bb1ae89cfae956aa
Fariha-Amin <fariha.amin@dsinnovators.com>
2024-08-30 14:21:05 +0600

    Search with employee name from directory added

--- cypress/e2e/orangehrm_e2e.cy.js
+++ cypress/e2e/orangehrm_e2e.cy.js
@@ -1,6 +1,7 @@
 const { faker } = require('@faker-js/faker');
 import MainMenu from '../pageObjects/mainMenu';
 import AddEmployee from '../pageObjects/addEmployee';
+import Directory from '../pageObjects/directory';
 
 describe('OrangeHRM End to End Testing', () => {
 
@@ -15,6 +16,13 @@ describe('OrangeHRM End to End Testing', () => {
     const symbols = faker.internet.password(1, false, /[\W_]/);
     return upperCase + lowerCase + numbers + symbols;
   }
+
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const username = firstName + lastName;
+  const fullName = firstName + " " + lastName;
+  const password = generatePassword();
+
 
   before(() => {
     // Load admin credentials
@@ -36,12 +44,6 @@ describe('OrangeHRM End to End Testing', () => {
   it('Creating a New Employee', () => {
     const mainMenu = new MainMenu();
     const addEmployee = new AddEmployee();
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
-    const username = firstName + lastName;
-    const fullName = firstName + " " + lastName;
-    const password = generatePassword();
-  
 
     cy.visit('/');
     cy.waitTillElementIsVisible('h6');
@@ -54,13 +56,13 @@ describe('OrangeHRM End to End Testing', () => {
     addEmployee.getUsername().type(username);
     addEmployee.getPassword().type(password);
     addEmployee.getConfirmPassword().type(password);
-    addEmployee.getEmployeeId().invoke('val').then(employeeId=>{
-    cy.writeFile(`cypress/fixtures/${employeeDataFile}`, {
-      username,
-      password,
-      employeeId
-    });
-  })
+    addEmployee.getEmployeeId().invoke('val').then(employeeId => {
+      cy.writeFile(`cypress/fixtures/${employeeDataFile}`, {
+        username,
+        password,
+        employeeId
+      });
+    })
     addEmployee.getSaveButton().click({ force: true });
     addEmployee.getToastMessage().should("have.text", "Successfully Saved");
     cy.waitTillElementIsVisible('h6');
@@ -75,13 +77,37 @@ describe('OrangeHRM End to End Testing', () => {
     mainMenu.getPIM().click()
     cy.waitTillElementIsVisible('h6');
     cy.get('h6').should("contain.text", 'PIM');
-    
-    cy.fixture(employeeDataFile).then((employee)=>{
-    addEmployee.getEmployeeId().type(employee.employeeId)
-    addEmployee.getSearchEmployeeButton().click({ force: true })
-    addEmployee.getTableCell().contains(employee.employeeId).should('be.visible')
+
+    cy.fixture(employeeDataFile).then((employee) => {
+      addEmployee.getEmployeeId().type(employee.employeeId)
+      addEmployee.getSearchEmployeeButton().click({ force: true })
+      addEmployee.getTableCell().contains(employee.employeeId).should('be.visible')
+    })
   })
-})
+
+  it('Search in Directory by Employee Name', () => {
+    cy.visit(lastUrl)
+    const mainMenu = new MainMenu();
+    const directory = new Directory();
+    mainMenu.getDirectory().click()
+    cy.waitTillElementIsVisible('h6');
+    cy.get('h6').should("contain.text", 'Directory');
+
+    cy.fixture(employeeDataFile).then((employee) => {
+    directory.getEmployeeNameField().type(firstName)
+    directory.getAutoCompleteDropdown().click()
+    directory.getSearchButton().click()
+    directory.getEmployeeCardHeader()
+    .invoke('text')
+    .then((text) => {
+      const normalizedText = text.replace(/\s+/g, ' ').trim(); 
+      expect(normalizedText).to.eq(fullName);
+    });
+
+    })
+
+    
+  })
 
   afterEach(() => {
     cy.url().then((url) => {
